3a30dd814b272abe6bda1a323e3ece4702d2381e
Andreas Wenzel <65829980+andreasmwenzel@users.noreply.github.com>
2021-03-19 16:54:03 -0700

    add merge sort

--- sorting.ts
+++ sorting.ts
@@ -1,7 +1,7 @@
 //insert sort
 
-let arr = new Array<number>(50);
-for(let i = 0; i<50; i++){
+let arr:number[] = new Array<number>(20);
+for(let i = 0; i<arr.length; i++){
   arr[i] = Math.floor(Math.random()*100)
 }
 
@@ -19,6 +19,40 @@ function insertSort(arr:Array<number>):void{
     arr[j+1] = key;
   }
 }
-console.log(arr);
-insertSort(arr);
-console.log(arr);
+// console.log(arr);
+// insertSort(arr);
+// console.log(arr);
+
+//merge sort
+//merge two arrays
+let a1 = [5,7,1];
+let a2 = [2,4];
+function merge(left:number[], right:number[]):number[]{
+  let merged = new Array<number>(left.length + right.length);
+  left.push(Infinity);
+  right.push(Infinity);
+  let li = 0;
+  let ri = 0;
+  for(let i =0; i<merged.length; i++){
+    if(left[li]<right[ri]){
+      merged[i] = left[li];
+      li = li + 1;
+    }else{
+      merged[i] = right[ri];
+      ri = ri + 1;
+    }
+  }
+  return(merged);
+}
+
+function mergeSort(arr:number[]):number[]{
+  //split
+  if(arr.length <= 1) return arr;
+  let m = Math.floor(arr.length/2);
+  let left = arr.slice(0,m);
+  let right = arr.slice(m, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(arr)
+console.log(mergeSort(arr));
